d699d0c403b43cf820dd38af1b04cd3320607fb3
Arthur Pons <unguestdev@gmail.com>
2020-05-15 21:10:58 +0200

    Setting Babel & Jasmine up !

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict';
+require('amd-loader');
+
+define(function (require, exports, module) {
+    'use strict';
+
+    console.log("INITIALIZING BOB EXTENSION");
+    
+    var CommandManager = brackets.getModule("command/CommandManager");
+    var Menus          = brackets.getModule("command/Menus");
+    //var EditorManager  = brackets.getModule("editor/EditorManager");
+    //var ProjectManager = brackets.getModule("project/ProjectManager");
+    //var FileUtils = brackets.getModule("file/FileUtils");    
+    //var DocumentManager = brackets.getModule("document/DocumentManager");    
+    //var NativeApp = brackets.getModule("utils/NativeApp");
+    //var Commands = brackets.getModule("command/Commands");
+    
+    
+    var wta_ID = "bob.wta"; // WTA = WebToolsActivation 
+    var wta_NAME = "Web Tools";
+    
+    
+    function wta() {
+        console.log("Executing Command WTA");
+    }
+
+
+    CommandManager.register(wta_NAME, wta_ID, wta);
+    
+    var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+    menu.addMenuDivider();
+    menu.addMenuItem(wta_ID);
+    
+});
