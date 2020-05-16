4f1d47b2fc96220ed17b9789f95d7dcb8f2ebfed
nadchif <nadchif@gmail.com>
2020-05-17 00:31:40 +0200

    working project config

---
+++ hashing/MD5.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* eslint-disable new-cap */
+/**
+ *  File: MD5.js
+ *  Author: Shankhanil <shankha.rik@gmail.com>
+ *  Description:  Encodes String --> MD5
+ *  MD5 is a one way encoding algorithm,
+ *  meaning we can only encode, and not decode.
+ *
+ */
+
+define(function (require, exports) {
+  var crypto = require('crypto-js');
+  var encodeToMD5 = function encodeToMD5(text) {
+    var hash = crypto.MD5(text).toString();
+    return hash;
+  };
+  exports.encodeToMD5 = encodeToMD5;
+});
---
+++ hashing/SHA1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* eslint-disable new-cap */
+/**
+ *  File: SHA1.js
+ *  Author: Arthur Pons <unguestdev@gmail.com> aka unguest on github
+ *  Description:  Encodes String --> SHA1
+ *  SHA1 is a one way encoding algorithm,
+ *  meaning we can only encode, and not decode.
+ *
+ */
+
+define(function (require, exports) {
+  var crypto = require('crypto-js');
+  var encodeToSHA1 = function encodeToSHA1(text) {
+    var hash = crypto.SHA1(text).toString();
+    return hash;
+  };
+  exports.encodeToSHA1 = encodeToSHA1;
+});
