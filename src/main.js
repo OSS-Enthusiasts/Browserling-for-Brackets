542b668ebf946b067ad51f2075d51b9a0a4bfc2c
nadchif <nadchif@gmail.com>
2020-05-16 22:47:38 +0200

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,32 +1,43 @@
-'use strict';
-require('amd-loader');
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+// eslint-disable-next-line max-len
+/* jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+// eslint-disable-next-line no-unused-vars
+/* global define, $, brackets */
 
+
+/**
+ *  File: Main.js
+ *  Author: github.com/OSS-Enthisiasts
+ *  Description:  This extension allows you to encode/decode selection
+ */
+
+// eslint-disable-next-line no-unused-vars
 define(function (require, exports, module) {
-    'use strict';
-	const Sentry = require('@sentry/node');
-	Sentry.init({ dsn: 'https://242a9a644d35453fbd84db17a22978ec@o315517.ingest.sentry.io/5243016' }); // Sentry error catching
-    console.log("INITIALIZING BOB EXTENSION");
-    var CommandManager = brackets.getModule("command/CommandManager");
-    var Menus          = brackets.getModule("command/Menus");
-    //var EditorManager  = brackets.getModule("editor/EditorManager");
-    //var ProjectManager = brackets.getModule("project/ProjectManager");
-    //var FileUtils = brackets.getModule("file/FileUtils");    
-    //var DocumentManager = brackets.getModule("document/DocumentManager");    
-    //var NativeApp = brackets.getModule("utils/NativeApp");
-    //var Commands = brackets.getModule("command/Commands");
-    
-    
-    var wta_ID = "bob.wta"; // WTA = WebToolsActivation 
-    var wta_NAME = "Web Tools";
-    function wta() {
-        console.log("Executing Command WTA");
-    }
+  const Sentry = require('@sentry/node');
+  Sentry.init({ dsn: 'https://242a9a644d35453fbd84db17a22978ec@o315517.ingest.sentry.io/5243016' }); // Sentry error catching
+
+  const CommandManager = brackets.getModule('command/CommandManager');
+  const Menus = brackets.getModule('command/Menus');
+  // var EditorManager  = brackets.getModule("editor/EditorManager");
+  // var ProjectManager = brackets.getModule("project/ProjectManager");
+  // var FileUtils = brackets.getModule("file/FileUtils");
+  // var DocumentManager = brackets.getModule("document/DocumentManager");
+  // var NativeApp = brackets.getModule("utils/NativeApp");
+  // var Commands = brackets.getModule("command/Commands");
 
 
-    CommandManager.register(wta_NAME, wta_ID, wta);
-    
-    var menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
-    menu.addMenuDivider();
-    menu.addMenuItem(wta_ID);
-    
+  const wtaId = 'bob.wta'; // WTA = WebToolsActivation
+  const wtaName = 'Web Tools';
+  function wta() {
+    // AirBnB standard does will throw errors when you leave console.log
+    // console.log('Executing Command WTA');
+  }
+
+
+  CommandManager.register(wtaName, wtaId, wta);
+
+  const menu = Menus.getMenu(Menus.AppMenuBar.VIEW_MENU);
+  menu.addMenuDivider();
+  menu.addMenuItem(wtaId);
 });
