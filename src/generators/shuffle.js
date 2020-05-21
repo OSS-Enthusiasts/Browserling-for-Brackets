b6377322c64a666c971f03f6cb1ae5a502bc462c
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-22 01:43:46 +0530

    introduced generators/shuffle

---
+++ src/generators/shuffle.js
@@ -0,0 +1,22 @@
+/**
+ *  File: PHPSerialize.js
+ *  Author: Shankhanil <shankha.rik@gmail.com>
+ *  Description:  Shuffles a string
+ */
+
+define((require, exports) => {
+
+  //    PHP Serializing string
+  const shuffle = function shuffle(input) {
+    let chars = input.split(""), strlen = chars.length;
+
+    for(var i = strlen - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = tmp;
+    }
+    return chars.join("");
+  };
+  exports.shuffle = shuffle;
+});
