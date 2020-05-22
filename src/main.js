82f14afcee952f27c866d2d58971a3af961e8fe7
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-22 12:29:44 +0530

    setting up a working menu

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ define((require, exports, module) => {
   // Sentry.init({ dsn: 'https://242a9a644d35453fbd84db17a22978ec@o315517.ingest.sentry.io/5243016' }); // Sentry error catching
 
   const CommandManager = brackets.getModule('command/CommandManager');
+  const EditorManager = brackets.getModule('editor/EditorManager');
   const Menus = brackets.getModule('command/Menus');
   // const EditorManager = brackets.getModule('editor/EditorManager');
 
@@ -29,14 +30,25 @@ define((require, exports, module) => {
 
   /** Sets up everything required for each category
   * Usage:
-  * @param id - the id of the tool category, ie 'wta',
+  * @param id - the id of the encodeTool category, ie 'wta',
   * @param categoryTitle - the title, ie 'Web Tools',
-  * @param categoryTools - array of the tools paired with the title and function
+  * @param categoryTools - array of the encodeTools paired with the title and function
   *                        ie [{
   *                               title: 'URI Encode',
-  *                               tool: require('./web/uri').encodeToURI,
+  *                               encodeTool: require('./web/uri').encodeToURI,
   *                           }];
   */
+  const convertInput = function convertInput(convertor) {
+    const editor = EditorManager.getFocusedEditor();
+    if (editor) {
+      // eslint-disable-next-line no-underscore-dangle
+      const selectedText = editor._codeMirror.getSelections();
+      const converted = convertor(selectedText[0]);
+      // eslint-disable-next-line no-underscore-dangle
+      editor._codeMirror.replaceSelections([converted]);
+    }
+  };
+
   const stageMenu = (id, categoryTitle, categoryTools) => {
     let isToolEnabled = false;
     CommandManager.register(categoryTitle, `oss.bob.${id}`, () => {
@@ -45,7 +57,8 @@ define((require, exports, module) => {
         categoryTools.forEach((item, index) => {
           CommandManager.register(item.title, `oss_bob_${id}${index}`, () => {
             // eslint-disable-next-line no-alert
-            alert('Execute conversion function here');
+            //            alert('Execute conversion function here');
+            convertInput(item.encodeTool);
           });
           subMenu.addMenuItem(`oss_bob_${id}${index}`, null, Menus.FIRST);
         });
@@ -65,12 +78,47 @@ define((require, exports, module) => {
   // Set up category menus here using this format
 
   const WEB_TOOLS = [{
-    title: 'URI Encode',
-    tool: require('./web/uri').encodeToURI,
-  }];
+    title: 'Strin --> URI',
+    encodeTool: require('./web/uri').encodeToURI,
+    decodeTool: require('./web/uri').decodeFromURI,
+  },
+  {
+    title: 'String --> Binary',
+    encodeTool: require('./web/bin').encodeToBin,
+    decodeTool: require('./web/bin').decodeFromBin,
+  },
+  {
+    title: 'String --> Hexadecimal',
+    encodeTool: require('./web/hexadecimal').encodeToHex,
+    decodeTool: require('./web/hexadecimal').decodeFromHex,
+  },
+  {
+    title: 'String --> PHPSerialize',
+    encodeTool: require('./web/PHPSerialize').encodeToPHPSerial,
+    decodeTool: require('./web/PHPSerialize').decodeFromPHPSerial,
+  },
+  {
+    title: 'NL --> <br>',
+    encodeTool: require('./web/nl2br').encodeNlToBr,
+    decodeTool: require('./web/nl2br').decodeNlFromBr,
+  },
+
+  ];
   const HASH_TOOLS = [{
-    title: 'Calc MD5',
-    tool: require('./hashing/MD5').encodeToURI,
+    title: 'Hash using MD5',
+    encodeTool: require('./hashing/MD5').encodeToMD5,
+  },
+  {
+    title: 'Hash using SHA1',
+    encodeTool: require('./hashing/SHA1').encodeToSHA1,
+  },
+  {
+    title: 'Hash using SHA256',
+    encodeTool: require('./hashing/SHA256').encodeToSHA256,
+  },
+  {
+    title: 'Hash using SHA512',
+    encodeTool: require('./hashing/SHA512').encodeToSHA512,
   }];
 
   stageMenu('web', 'Web Tools', WEB_TOOLS);
