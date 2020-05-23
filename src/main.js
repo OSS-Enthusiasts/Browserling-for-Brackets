7699696e1991d30c8c3c19546416adf8e4e37dfc
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-05-23 19:30:21 +0530

    updated menu look

--- src/main.js
+++ src/main.js
@@ -31,10 +31,10 @@ define((require, exports, module) => {
   /** Sets up everything required for each category
   * Usage:
   * @param id - the id of the encodeTool category, ie 'wta',
-  * @param categoryTitle - the title, ie 'Web Tools',
-  * @param categoryTools - array of the encodeTools paired with the title and function
+  * @param categoryTitle - the encoderTitle, ie 'Web Tools',
+  * @param categoryTools - array of the encodeTools paired with the encoderTitle and function
   *                        ie [{
-  *                               title: 'URI Encode',
+  *                               encoderTitle: 'URI Encode',
   *                               encodeTool: require('./web/uri').encodeToURI,
   *                           }];
   */
@@ -54,13 +54,21 @@ define((require, exports, module) => {
     CommandManager.register(categoryTitle, `oss.bob.${id}`, () => {
       if (!isToolEnabled) {
         const subMenu = Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addSubMenu(categoryTitle, `OSS_BOB_MENU_${id}`);
+        subMenu.addMenuDivider();
         categoryTools.forEach((item, index) => {
-          CommandManager.register(item.title, `oss_bob_${id}${index}`, () => {
+          CommandManager.register(item.encoderTitle, `oss_bob_enc_${id}${index}`, () => {
             // eslint-disable-next-line no-alert
             //            alert('Execute conversion function here');
             convertInput(item.encodeTool);
           });
-          subMenu.addMenuItem(`oss_bob_${id}${index}`, null, Menus.FIRST);
+          subMenu.addMenuItem(`oss_bob_enc_${id}${index}`, null, Menus.FIRST);
+
+          if (item.decoderTitle) {
+            CommandManager.register(item.decoderTitle, `oss_bob_dec_${id}${index}`, () => {
+              convertInput(item.decodeTool);
+            });
+            subMenu.addMenuItem(`oss_bob_dec_${id}${index}`, null, Menus.LAST);
+          }
         });
         isToolEnabled = true;
         CommandManager.get(`oss.bob.${id}`).setChecked(true);
@@ -78,46 +86,51 @@ define((require, exports, module) => {
   // Set up category menus here using this format
 
   const WEB_TOOLS = [{
-    title: 'Strin --> URI',
+    encoderTitle: 'String to URI',
     encodeTool: require('./web/uri').encodeToURI,
+    decoderTitle: 'URI to String',
     decodeTool: require('./web/uri').decodeFromURI,
   },
   {
-    title: 'String --> Binary',
+    encoderTitle: 'String to Binary',
     encodeTool: require('./web/bin').encodeToBin,
+    decoderTitle: 'Binary to String',
     decodeTool: require('./web/bin').decodeFromBin,
   },
   {
-    title: 'String --> Hexadecimal',
+    encoderTitle: 'String to Hexadecimal',
     encodeTool: require('./web/hexadecimal').encodeToHex,
+    decoderTitle: 'Hexadecimal to String',
     decodeTool: require('./web/hexadecimal').decodeFromHex,
   },
   {
-    title: 'String --> PHPSerialize',
+    encoderTitle: 'String to PHPSerialize',
     encodeTool: require('./web/PHPSerialize').encodeToPHPSerial,
+    decoderTitle: 'PHPSerialize to String',
     decodeTool: require('./web/PHPSerialize').decodeFromPHPSerial,
   },
   {
-    title: 'NL --> <br>',
+    encoderTitle: 'NL to <br>',
     encodeTool: require('./web/nl2br').encodeNlToBr,
+    decoderTitle: '<br> to NL',
     decodeTool: require('./web/nl2br').decodeNlFromBr,
   },
 
   ];
   const HASH_TOOLS = [{
-    title: 'Hash using MD5',
+    encoderTitle: 'Hash using MD5',
     encodeTool: require('./hashing/MD5').encodeToMD5,
   },
   {
-    title: 'Hash using SHA1',
+    encoderTitle: 'Hash using SHA1',
     encodeTool: require('./hashing/SHA1').encodeToSHA1,
   },
   {
-    title: 'Hash using SHA256',
+    encoderTitle: 'Hash using SHA256',
     encodeTool: require('./hashing/SHA256').encodeToSHA256,
   },
   {
-    title: 'Hash using SHA512',
+    encoderTitle: 'Hash using SHA512',
     encodeTool: require('./hashing/SHA512').encodeToSHA512,
   }];
 
