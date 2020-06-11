5d6f34cac3907707f6067b2a9dd7b7382491ff7d
Shankhanil Ghosh <shankha.rik@gmail.com>
2020-06-12 01:22:12 +0530

    added rudimentary password strength module

--- spec/password.spec.js
+++ spec/password.spec.js
@@ -8,8 +8,9 @@ require('amd-loader');
 define((require) => {
   describe('src/generators/password.js =>', () => {
     const { passwordGen } = require('../src/generators/password.js');
+    const { strength } = require('../src/generators/password.js');
 
-    describe('generates string of length 8-12 => ', () => {
+    describe('Password module generates password of length 8-12 => ', () => {
       it('should generate a password of length 8-12 over 100 different passwords', () => {
         let password;
         for (let i = 1; i <= 100; i += 1) {
@@ -21,7 +22,13 @@ define((require) => {
     });
     describe('Strengh of password test => ', () => {
       it('Should generate a strong password', () => {
-        // expect(true).toBe(true);
+        expect(true).toBe(true);
+      });
+    });
+    describe('Checks the strength of password', () => {
+      it('Should return true for a strong password', () => {
+        const password = 'aB2+cc_sD';
+        expect(strength(password)).toBe(true);
       });
     });
   });
--- src/generators/password.js
+++ src/generators/password.js
@@ -6,7 +6,7 @@
 
 define((require, exports) => {
   //    PHP Serializing string
-  const passwordGen = function shuffle() {
+  const passwordGen = function passwordGen() {
     const length = Math.random() * (12 - 8) + 8;
     const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&_*+-/';
     let password = '';
@@ -16,7 +16,28 @@ define((require, exports) => {
     return password;
   };
   const strength = function strength(password) {
-    return `${password}NA`;
+    let isLong = false; let hasLower = false; let hasUpper = false; let hasNum = false; let
+      hasSpC = false;
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&_*+-/';
+    if (password.length >= 6) {
+      isLong = true;
+    }
+    for (let i = 0; i < password.length; i += 1) {
+      if (hasLower === false && charset.slice(0, 26).includes(password.charAt(i))) {
+        hasLower = true;
+      }
+      if (hasUpper === false && charset.slice(26, 26 + 26).includes(password.charAt(i))) {
+        hasUpper = true;
+      }
+      if (hasNum === false && charset.slice(52, 62).includes(password.charAt(i))) {
+        hasNum = true;
+      }
+
+      if (hasSpC === false && charset.slice(62, charset.length).includes(password.charAt(i))) {
+        hasSpC = true;
+      }
+    }
+    return isLong && hasLower && hasUpper && hasNum && hasSpC;
   };
   exports.passwordGen = passwordGen;
   exports.strength = strength;
